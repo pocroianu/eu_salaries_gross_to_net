eb5ca832e150ad42abea8157d8c54f8a1597b530
Geani Pocroianu <nicolaepocroianu@waterford.org>
2025-06-14 03:13:27 +0300

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Base path for GitHub Pages deployment
+  base: '/eu_salaries_gross_to_net/',
 })
